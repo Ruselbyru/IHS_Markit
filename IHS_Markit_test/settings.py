d05be8d4923a383cde905f888e43cec1fd1fc423
ruselbyru <r.schetko@gmail.com>
2021-11-02 16:39:31 +0300

    this is worked

--- IHS_Markit_test/settings.py
+++ IHS_Markit_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5x7ql$r9(u^%2@ftk++)r(#vsr%uuy5ut4t-c%8h45&h&-afy)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
